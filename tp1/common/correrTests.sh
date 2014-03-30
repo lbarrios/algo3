deb6c9246e98bdb4e7b78333f068a9da4d49c975
Ezequiel Barrios <lbarrios@users.noreply.github.com>
2014-03-30 23:59:12 +0000

    correrTest.sh movido a la carpeta common y linkeado desde las otras

---
+++ tp1/common/correrTests.sh
@@ -0,0 +1,154 @@
+#!/usr/bin/env bash
+EXE=${PWD##*/}
+INPUT_FOLDER=input
+OUTPUT_FOLDER=output
+VERBOSE=false
+TIME=false
+HELP=false
+DEFAULT=true
+DESDE=false
+HASTA=false
+platform='unknown'
+MAKE=make
+unamestr=`uname`
+function pause(){
+  clear
+  echo "
+
+---------------------- ATENCION -----------------------------------------------
+"
+  echo "$*"
+  echo "
+-------------------------------------------------------------------------------"
+  read -p "Presione la tecla [Enter] para continuar."
+}
+if [[ "$unamestr" == 'Linux' ]]; then
+  platform='linux'
+elif [[ "$unamestr" == 'FreeBSD' ]]; then
+  platform='freebsd'
+  MAKE=gmake
+fi
+
+if [ $# = 0 ]; then
+  echo "Se deben introducir parámetros para ejecutar este script.
+Para más información, ejecutar con el parámetro --help.
+Chau."
+  exit 0
+fi
+
+for i in "$@"
+do
+case $i in
+  -v|--verbose)
+  VERBOSE=true
+  shift
+  ;;
+  -t|--time|--tiempo)
+  TIME=true
+  shift
+  ;;
+  --desde=*)
+  DESDE="${i#*=}"
+  shift
+  ;;
+  --hasta=*)
+  HASTA="${i#*=}"
+  shift
+  ;;
+  -h|--help)
+  HELP=true
+  shift
+  ;;
+  #-b=*|--blabla=*)
+  #BLABLA="${i#*=}"
+  #shift
+  #;;
+  *)
+    echo "Parámetro no reconocido.
+Chau."
+  exit -1
+  ;;
+esac
+done
+
+
+if [ $TIME = true ]; then
+  EXE=$EXE.time
+fi
+
+
+if [ $HELP = true ]; then
+  echo "
+El script corre el ejecutable EXE desde la carpeta INPUT_FOLDER
+y los guarda con el mismo nombre en la carpeta OUTPUT_FOLDER.
+
+Valores por defecto:
+    INPUT_FOLDER: input
+    OUTPUT_FOLDER: output
+
+Opciones:
+  --help | -h:
+      Muestra este mensaje de ayuda.
+
+  --verbose | -v:
+      Muestra el output obtenido al correr los tests.
+
+  --time | --tiempo | -t:
+      Realiza mediciones de tiempo.
+      Los archivos se guardarán con la extension '.time'
+"
+  exit 0
+fi
+
+
+#                                                                              #
+#                                                                              #
+################################################################################
+#                                                                              #
+################################################################################
+#                                                                              #
+################################################################################
+#                                                                              #
+################################################################################
+#                                                                              #
+#                                                                              #
+
+clear
+$MAKE $EXE
+if [ $? = 0 ]; then
+  if [ $DESDE = false ] && [ $HASTA = false ]; then
+    pause "Se correra el programa para TODOS los archivos de input 
+que se encuentren en la carpeta '$INPUT_FOLDER'.
+
+Los resultados serán volcados a la carpeta '$OUTPUT_FOLDER'
+             Esto puede demorar mucho.
+
+Puede obtener ayuda sobre el programa ejecutando --help.
+Si desea cancelar la ejecución, ahora es el momento."
+    clear
+  fi
+  INPUTS=$INPUT_FOLDER/*.txt
+  for INPUT in $INPUTS
+  do
+      OUTPUT=$OUTPUT_FOLDER/${INPUT##*/}
+      EXE_PARAM="-i $INPUT -o $OUTPUT"
+      if [ $TIME = true ]; then
+        EXE_PARAM="$EXE_PARAM -t $OUTPUT.time"
+      fi
+      echo "EJECUTANDO: ./$EXE $EXE_PARAM"
+      ./$EXE $EXE_PARAM
+      if [ $? != 0 ]; then
+           echo "HUBO UN ERROR AL CORRER EL INPUT ${INPUT##*/}!!!"
+      else
+        if [ $VERBOSE == true ]; then
+          cat $OUTPUT
+        fi
+      fi
+  done
+  echo ""
+  echo ""
+  echo ""
+  echo "                                         Fin de las pruebas"
+else
+    echo "El programa no compiló bien!!!"
+fi
