5bd7b5fd668af9a25a7eda9a078f726acc083965
Ezequiel Barrios <lbarrios@users.noreply.github.com>
2014-05-06 06:03:53 +0000

    grrr

--- tp2/ej2/compilar.sh
+++ tp2/ej2/compilar.sh
@@ -1,12 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 g++ -O2 ej2.cpp ../common/Timer.cpp -o ej2
 g++ -O2 -DTIME ej2.cpp ../common/Timer.cpp -o ej2.time
 g++ -DDEBUG ej2.cpp ../common/Timer.cpp -o ej2.debug
-
-
-
-   
-
-   
-
