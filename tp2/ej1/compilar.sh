74ba7ce2bb3552e404bd67e0aa8f694a8fc1d71b
Ezequiel Barrios <lbarrios@users.noreply.github.com>
2014-05-06 23:11:22 +0000

    ...

--- tp2/ej1/compilar.sh
+++ tp2/ej1/compilar.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 g++ -O2 ej1.cpp ../common/Timer.cpp -o ej1
-g++ -O2 -DTIME ej1.cpp ../common/Timer.cpp -o ej1.time
+g++ -O2 -DTIME ej1.cpp ../common/Timer.cpp -o ej1.time -lrt
